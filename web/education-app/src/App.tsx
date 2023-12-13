6729e44c0001f23f15b49fdaf3a46034a8afd4e5
Rodrigo Carneiro Andrade <rod_andrade_@MacBook-Pro-de-Rodrigo.local>
2023-12-13 18:57:51 -0300

    create repository

---
+++ web/education-app/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <>
+      <main className="h-screen bg-black flex justify-center">
+        <div className="w-full max-w-7xl py-24">
+          <h1 className="text-xl text-slate-100 ">Dados do aluno</h1>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
