c362557a1fae465e9d6c7537d68b2a251470c773
Rodrigo Carneiro Andrade <rod_andrade_@MacBook-Pro-de-Rodrigo.local>
2023-12-14 15:21:19 -0300

    fix: changes table

--- web/education-app/src/pages/Results/index.tsx
+++ web/education-app/src/pages/Results/index.tsx
@@ -77,9 +77,9 @@ export function Results() {
         )}
       </section>
       <section className="space-y-4">
-        <div className="mt-8 flex flex-col">
-          <div className="-my-2 -mx-4 sm:-mx-6 lg:-mx-8">
-            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+        <div className="mt-8 flex flex-col overflow-hidden ">
+          <div className="overflow-x-auto -my-2 -mx-4 sm:-mx-6 lg:-mx-8">
+            <div className="inline-block  min-w-full py-2 align-middle md:px-6 lg:px-8">
               <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
                 <table
                   className="min-w-full divide-y divide-gray-300"
