1ba57229a7035ec901851535634b3dc448e3931e
Rodrigo Carneiro Andrade <rod_andrade_@MacBook-Pro-de-Rodrigo.local>
2023-12-14 15:31:54 -0300

    fix: word change

--- web/education-app/src/pages/Results/index.tsx
+++ web/education-app/src/pages/Results/index.tsx
@@ -97,7 +97,7 @@ export function Results() {
                         scope="col"
                         className="sticky top-0 z-10 hidden border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell"
                       >
-                        matéria
+                        componente
                       </th>
                       <th
                         scope="col"
